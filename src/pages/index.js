2c5be19bf0a74ef3a58bf4d6108d0394595ac3a0
Denny <d3portillo@gmail.com>
2019-10-28 12:43:29 -0600

    adds yandex verification

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import Container from "../layouts/Conainer"
 import Helmet from "react-helmet"
-const IndexPage = () => (
+export default () => (
   <Container className="flex flex-col p-8 items-center justify-center min-h-screen">
-    <Helmet title="Nodeschool San Miguel" />
+    <Helmet title="Nodeschool San Miguel">
+      <meta name="yandex-verification" content="a3512b4ed07068b8" />
+    </Helmet>
+
     <div className="text-2xl uppercase font-bold mb-6 underline">
       Construyendo sitio
     </div>
@@ -11,5 +14,3 @@ const IndexPage = () => (
     <p className="mt-4">Here will be unicorns!</p>
   </Container>
 )
-
-export default IndexPage
