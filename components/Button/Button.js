742b6e6f82d0d7eec79bba1791fde982fc478bb4
Daniel Castillo <dbocastillo@gmail.com>
2020-10-19 21:38:58 -0600

    includes a button that needs improvement

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,19 +1,46 @@
-export function Button(props){
-    const {className = '', ...restProps} = props;
-    return(
-        <button className={`bg-yellow text-black font-black  text-xl px-6 py-5 hover:shadow-yellow ${className} `} {...restProps}>
+import React, {useState} from "react"
+import type from "prop-types"
+
+
+
+/**
+ * The only true button.
+ *
+ * @version 0.0.1
+ * @author [Angel Castillo](https://github.com/x905)
+ * @prop {string} className
+ * @prop {bool} secondary
+ * @prop {string} children
+ */
+const Button = (props) => {
+    const [secondary] = useState( props.secondary)
+    const [className] = useState( props.className)
+    const [children] = useState( props.children)
+    
+
+    return (
+        <button className={`text-black font-black text-xl px-6 py-5  ${secondary ?' bg-grey hover:shadow-grey':'bg-yellow hover:shadow-yellow'}  ${className} `}   >
+            {children}
         </button>
-
     )
 }
 
 
-export function ButtonGray(props){
-    const {className = '', ...restProps} = props;
-    return(
 
-        <Button className={`bg-grey hover:shadow-grey ${className}`} {...restProps}>
-        </Button>
-    )
+Button.propTypes = {
+  
+    className: type.string,
+  
+    children: type.node,
+
+    secondary : type.bool,
+    /**
+     * Gets called when the user clicks on the button
+     *
+     * @param {SyntheticEvent} event The react `SyntheticEvent`
+     * @param {Object} allProps All props of this Button
+     */
+    onClick : type.func
 }
 
+export default Button
